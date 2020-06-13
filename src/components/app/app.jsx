8bc1376a382ee897f050603f935790f3569a1ebd
pgalkevich <pgalkevich@gmail.com>
2020-06-13 22:03:11 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {offersCount} = props;
+  const {data} = props;
 
   return (
-    <Main offersCount={offersCount}/>
+    <Main data={data}/>
   );
+};
+
+App.propTypes = {
+  data: PropTypes.shape({
+    offersCount: PropTypes.number.isRequired,
+    offerNames: PropTypes.arrayOf(
+        PropTypes.string.isRequired
+    ).isRequired
+  }).isRequired
 };
 
 export default App;
