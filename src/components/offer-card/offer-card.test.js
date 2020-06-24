b5bb76d1f54d365c8f5f8de2ff5a1d2808d8e13b
pgalkevich <pgalkevich@gmail.com>
2020-06-24 12:52:46 +0300

    module3-task2

--- src/components/offer-card/offer-card.test.js
+++ src/components/offer-card/offer-card.test.js
@@ -7,8 +7,30 @@ const offer = {
   imgSrc: `img/apartment-01.jpg`,
   premium: true,
   price: 120,
-  rating: 4,
+  rating: 4.8,
   type: `Apartment`,
+  bedroomsCount: 3,
+  maxCapacity: 4,
+  photos: [
+    `room.jpg`,
+    `apartment-01.jpg`,
+    `apartment-02.jpg`,
+    `apartment-03.jpg`,
+    `studio-01.jpg`,
+    `apartment-01.jpg`
+  ],
+  features: [
+    `Wi-Fi`,
+    `Washing machine`,
+    `Towels`,
+    `Heating`,
+    `Coffee machine`,
+    `Baby seat`,
+    `Kitchen`,
+    `Dishwasher`,
+    `Cabel TV`,
+    `Fridge`
+  ]
 };
 
 it(`OfferCard is rendered correctly`, () => {
